f0800d4601d8a63e9dd40c1ad5ddd30d2966959d
natalie king <108791553+nataliemakhijani@users.noreply.github.com>
2024-09-09 14:19:17 -0400

    clean up lesson 2 stuff and add more comments

--- unit-1/lesson-2/assignment.py
+++ unit-1/lesson-2/assignment.py
@@ -1,42 +1,44 @@
 from unit1lesson2 import *
 
+# smallest number
 smallest = number_list[0]
-
 for number in number_list:
     if number < smallest: # if it's smaller than the current smallest, it becomes the new number to compare against
         smallest = number
-
 print("smallest number", smallest) # 175
-# true
+# can this be done as a one-liner? probably.
+# could also be done recursively
 
+# smallest number above 500
 numbers_above_500 = [num for num in number_list if num > 500]
-
 smallest = numbers_above_500[0]
-
 for number in numbers_above_500:
     if number < smallest: # if it's smaller than the current smallest, it becomes the new number to compare against
         smallest = number
-
 print("smallest number above 500", smallest) # 501
 
+# smallest even
 evens = [num for num in number_list if num % 2 == 0]
-
 smallest = evens[0]
-
 for number in evens:
     if number < smallest: # if it's smaller than the current smallest, it becomes the new number to compare against
         smallest = number
-
 print("smallest even", smallest) #176
 
+# find last word (alpha) in list
 last_word = sorted(word_list)[-1]
+print("last_word:", last_word) # violation
 
-print(last_word) # violation
+# if we do it without sorted
+last_word = word_list[0]
+for word in word_list:
+    if word > last_word:
+        last_word = word
+print("last (w/o sorted):", last_word)
 
+# longest word
 longest = ""
-
 for word in word_list:
     if len(word) > len(longest):
         longest = word
-
 print("longest word is", longest) # rehabilitation
