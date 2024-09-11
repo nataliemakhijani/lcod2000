ffaaa0ca45cc20d4e9fb64983d56448ea57f28d4
natalie king <108791553+nataliemakhijani@users.noreply.github.com>
2024-09-11 15:44:24 -0400

    classwork from lesson 3

--- unit-1/lesson-3/classwork.py
+++ unit-1/lesson-3/classwork.py
@@ -1,2 +1,11 @@
 # 2024-09-09
 # Natalie Makhijani
+import sys # builtin
+from PIL import Image
+
+args = sys.argv
+args.pop(0) # remove first arg
+
+image = Image.open(args[0])
+
+print(image.format_description)
